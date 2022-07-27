3d4547c7087d2d38fd06ded669c9a5cca62c91ac
Телицын Никита <dmtvn.labs@yandex.ru>
2022-07-27 13:08:30 +0300

    Connected bd

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 require('dotenv').config()
 const app = require('./app')
+const db = require('./db')
 const port = process.env.PORT || 5200
 
-app.listen(port, () => console.log(`Сервер запущен на порту ${port}`))
+const start = async () => {
+    try {
+        await db.authenticate().then(() => console.log('БД подключина'))
+        await db.sync()
+        app.listen(port, () => console.log(`Сервер запущен на порту ${port}`))
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
+start()
