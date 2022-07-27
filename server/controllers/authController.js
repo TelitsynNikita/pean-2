1eccd26708fcac7dd4c1333149c9e9b35c2e1aa4
Телицын Никита <dmtvn.labs@yandex.ru>
2022-07-27 18:47:59 +0300

    Added registration and login controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,53 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.JWT_SECRET_KEY,
+        {expiresIn: '1h'}
+    )
+}
 
 class AuthController {
     async login(req, res) {
         const {email, password} = req.body
         const user = await User.findOne({where: {email}})
 
-        if (user) {
-            res.json({user})
+        if (!user) {
+            res.status(500).json({message: 'Пользователь с таким email не найден'})
         }
+
+        const comparePassword = await bcrypt.compare(password, user.password)
+
+        if (!comparePassword) {
+            return res.status(404).json({message: 'Неверно введён пароль'})
+        }
+
+        const token = generateJwt(user.id, user.email, user.role)
+
+        return res.json({token})
     }
 
     async registration(req, res) {
-        const {email, password} = req.body
-        const candidate = await User.create({email, password})
-        res.json({candidate})
+        const {email, password, role} = req.body
+
+        if (!email || !password) {
+            return res.status(500).json({message: 'Некорректный email или password'})
+        }
+
+        const candidate = await User.findOne({where: {email}})
+
+        if (candidate) {
+            return res.status(404).json({message: 'Пользователь с таким email уже существует'})
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const token = generateJwt(user.id, user.email, user.role)
+
+        return res.json({token})
     }
 }
 
